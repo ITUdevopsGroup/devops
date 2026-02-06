d4844777711709b18a4330c730c6455993ebdcc2
Anders Nielsen <andersfrimann@hotmail.dk>
2026-02-06 22:55:39 +0100

    React timeline page initial draft

--- migration_nextjs/minitwit-frontend/app/page.tsx
+++ migration_nextjs/minitwit-frontend/app/page.tsx
@@ -1,65 +1,117 @@
+'use client'
+
 import Image from "next/image";
+import * as http from 'http';
+import { JSX, Key, SetStateAction,useEffect } from "react";
+import { useState } from 'react';
+
+
+
+var host = "172.30.160.1"
+var path = ""
+var preparePath = ""
+
+
+interface message {
+  id: any;
+  author_id: any;
+  text: any;
+  pub_date: string;
+  flagged: string;
+  username: string;
+}
+  
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+  const [items, setItems] = useState(Array<message>);
+  const [refetch, setRefetch] = useState(false);
+
+    useEffect(() => { 
+    testfunc()
+  }, [refetch]);
+
+    async function testfunc(){
+      let api = await fetch('http://desktop-h4nlgfr:5001')
+      let apijson = await api.json()
+      setItems(apijson.data);
+      
+      
+  }
+  console.log(items)
+
+  
+ return (
+    <div className="public">
+      Public Timeline
+      <ul>
+        {items?.map((item) => (
+          <li key={item.id}>
+                <img src="{{ message.email|gravatar(size=48) }}"></img>
+                <p>
+                  <strong><a href="{{ url_for('user_timeline', username=message.username)
+                  }}">{item.username }</a></strong>
+                </p>
+                { item.text}
+                  <small>&mdash; {item.pub_date }</small>
+                
+              </li>
+        ))}
+    </ul>
+      
     </div>
   );
+}
+
+function processData( data: string) {
+        console.log("data")
+        console.log(JSON.parse(data).data)
+        return JSON.parse(data).data
+}
+
+function get(callback: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }): JSX.Element[] {
+    let opts = {
+        'host': host,
+        'port': 5001,
+        'path': `/${path}`
+    };
+    var result:Array<message> = []
+    var listItems:JSX.Element[] = []
+    http.get(opts, (r: http.IncomingMessage): void => {
+        let data = '';
+        r.on('data', (chunk: string): void => {
+            data += chunk;
+            
+        });
+        r.on('end', (): JSX.Element[] =>{
+            console.log('Response has ended');
+            
+            result = JSON.parse(data).data
+            console.log(result);
+            listItems = result.map((message) =>
+            <li key={message.id}>
+              <img src="{{ message.email|gravatar(size=48) }}"></img>
+              <p>
+                <strong><a href="{{ url_for('user_timeline', username=message.username)
+                }}">{message.username }</a></strong>
+              </p>
+              { message.text}
+                <small>&mdash; {message.pub_date }</small>
+              
+            </li>
+            ); 
+            callback(false)
+            //console.log(listItems);
+            
+            return listItems
+
+        });
+        r.on('error', (err): void => {
+            console.log('Following error occured during request:\n');
+            console.log(err);
+        });
+        
+    }).end();
+   
+    
+    return listItems
 }
