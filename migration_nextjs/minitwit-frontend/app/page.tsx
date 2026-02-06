38ee0f119a0e2eb1ecbdf2307636eebe74723a10
Anders Nielsen <andersfrimann@hotmail.dk>
2026-02-06 23:02:18 +0100

    React timeline page initial draft

--- migration_nextjs/minitwit-frontend/app/page.tsx
+++ migration_nextjs/minitwit-frontend/app/page.tsx
@@ -39,7 +39,6 @@ export default function Home() {
   }
   console.log(items)
 
-  
  return (
     <div className="public">
       Public Timeline
@@ -60,58 +59,4 @@ export default function Home() {
       
     </div>
   );
-}
-
-function processData( data: string) {
-        console.log("data")
-        console.log(JSON.parse(data).data)
-        return JSON.parse(data).data
-}
-
-function get(callback: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }): JSX.Element[] {
-    let opts = {
-        'host': host,
-        'port': 5001,
-        'path': `/${path}`
-    };
-    var result:Array<message> = []
-    var listItems:JSX.Element[] = []
-    http.get(opts, (r: http.IncomingMessage): void => {
-        let data = '';
-        r.on('data', (chunk: string): void => {
-            data += chunk;
-            
-        });
-        r.on('end', (): JSX.Element[] =>{
-            console.log('Response has ended');
-            
-            result = JSON.parse(data).data
-            console.log(result);
-            listItems = result.map((message) =>
-            <li key={message.id}>
-              <img src="{{ message.email|gravatar(size=48) }}"></img>
-              <p>
-                <strong><a href="{{ url_for('user_timeline', username=message.username)
-                }}">{message.username }</a></strong>
-              </p>
-              { message.text}
-                <small>&mdash; {message.pub_date }</small>
-              
-            </li>
-            ); 
-            callback(false)
-            //console.log(listItems);
-            
-            return listItems
-
-        });
-        r.on('error', (err): void => {
-            console.log('Following error occured during request:\n');
-            console.log(err);
-        });
-        
-    }).end();
-   
-    
-    return listItems
 }
