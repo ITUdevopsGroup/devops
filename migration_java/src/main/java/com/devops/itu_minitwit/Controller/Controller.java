f6516a1e4cd5e66cc604c727d8b172b949152177
Anders Nielsen <andersfrimann@hotmail.dk>
2026-02-09 17:31:26 +0100

    migration java

--- migration_java/src/main/java/com/devops/itu_minitwit/Controller/Controller.java
+++ migration_java/src/main/java/com/devops/itu_minitwit/Controller/Controller.java
@@ -1,6 +1,10 @@
 package com.devops.itu_minitwit.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devops.itu_minitwit.Database.DatabaseService;
@@ -26,8 +30,19 @@ public class Controller {
     PublicDataContainer data = databaseService.getPublicData();
     ObjectMapper mapper = new ObjectMapper();
     String result = mapper.writeValueAsString(data);
-    log.info(result);
+    //log.info(result);
     return result;
   }
+
+  @RequestMapping(value="user", method = RequestMethod.GET)
+public @ResponseBody String getItem(@RequestParam("user") int user) throws JsonProcessingException{
+
+    log.info("GET: /user");
+    PublicDataContainer data = databaseService.getUserData(user);
+    ObjectMapper mapper = new ObjectMapper();
+    String result = mapper.writeValueAsString(data);
+    log.info(result);
+    return result;
+}
 
 }
--- migration_java/src/main/java/com/devops/itu_minitwit/Database/DatabaseService.java
+++ migration_java/src/main/java/com/devops/itu_minitwit/Database/DatabaseService.java
@@ -3,7 +3,9 @@ package com.devops.itu_minitwit.Database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -13,6 +15,8 @@ import com.devops.itu_minitwit.Json.PublicDataRecord;
 
 public class DatabaseService {
     private final String PUBLIC_SQL = "select message.*, user.* from message, user where message.flagged = 0 and message.author_id = user.user_id order by message.pub_date desc limit 30";
+    private final String USER_SQL = "select message.*, user.* from message, user where message.flagged = 0 and message.author_id = user.user_id and (user.user_id = ? or user.user_id in (select whom_id from follower where who_id = ?)) order by message.pub_date desc limit 30";
+
     private static final Logger log = LogManager.getLogger();
     private static final String databasePath = "jdbc:sqlite:minitwit.db";
 
@@ -40,7 +44,36 @@ public class DatabaseService {
             ArrayList<PublicDataRecord> data = new ArrayList<PublicDataRecord>();
             while (rs.next()) {
                 PublicDataRecord record = new PublicDataRecord(rs.getInt("message_id"), rs.getString("author_id"),
-                        rs.getString("text"), rs.getInt("pub_date"), rs.getInt("flagged"));
+                        rs.getString("text"), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                          .format(new Date(rs.getInt("pub_date") * 1000L)), rs.getInt("flagged"),
+                    rs.getInt("user_id"),rs.getString("username"),rs.getString("email"),rs.getString("pw_hash"));
+                data.add(record);
+            }
+            PublicDataContainer result = new PublicDataContainer(data);
+            conn.close();
+            return result;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public PublicDataContainer getUserData(int user) {
+    log.info("Querying user data records");
+        try (
+            var conn = DriverManager.getConnection(databasePath);
+            var pstmt = conn.prepareStatement(USER_SQL)) {
+            pstmt.setInt(1, user);
+            pstmt.setInt(2, user);
+
+            var rs = pstmt.executeQuery();
+            ArrayList<PublicDataRecord> data = new ArrayList<PublicDataRecord>();
+
+            while (rs.next()) {
+                PublicDataRecord record = new PublicDataRecord(rs.getInt("message_id"), rs.getString("author_id"),
+                        rs.getString("text"), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+                          .format(new Date(rs.getInt("pub_date") * 1000L)), rs.getInt("flagged"),
+                    rs.getInt("user_id"),rs.getString("username"),rs.getString("email"),rs.getString("pw_hash"));
                 data.add(record);
             }
             PublicDataContainer result = new PublicDataContainer(data);
