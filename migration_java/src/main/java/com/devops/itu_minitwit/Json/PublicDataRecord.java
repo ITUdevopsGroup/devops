90e7c4eaa2050b71f9de0e01d0c958c32bf31a2b
Anders Nielsen <andersfrimann@hotmail.dk>
2026-02-09 17:31:26 +0100

    migration java

--- migration_java/src/main/java/com/devops/itu_minitwit/Json/PublicDataRecord.java
+++ migration_java/src/main/java/com/devops/itu_minitwit/Json/PublicDataRecord.java
@@ -4,17 +4,55 @@ public class PublicDataRecord {
     private int messageId;
     private String authorId;
     private String text;
-    private int pubDate;
+    private String pubDate;
     private int flagged;
+    private int userId;
+    private String username;
+    private String email;
+    private String pwHash;
+
         
     public PublicDataRecord() {
     }
-    public PublicDataRecord(int messageId, String authorId, String text, int pubDate, int flagged) {
+
+    
+    public PublicDataRecord(int messageId, String authorId, String text, String pubDate, int flagged, int userId,
+            String username, String email, String pwHash) {
         this.messageId = messageId;
         this.authorId = authorId;
         this.text = text;
         this.pubDate = pubDate;
         this.flagged = flagged;
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.pwHash = pwHash;
+    }
+
+
+    public int getUserId() {
+        return userId;
+    }
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPwHash() {
+        return pwHash;
+    }
+    public void setPwHash(String pwHash) {
+        this.pwHash = pwHash;
     }
     public int getMessageId() {
         return messageId;
@@ -34,10 +72,10 @@ public class PublicDataRecord {
     public void setText(String text) {
         this.text = text;
     }
-    public int getPubDate() {
+    public String getPubDate() {
         return pubDate;
     }
-    public void setPubDate(int pubDate) {
+    public void setPubDate(String pubDate) {
         this.pubDate = pubDate;
     }
     public int getFlagged() {
