1000bdcfb3519899b26b1a60cfc766e521c10912
Anders Nielsen <andersfrimann@hotmail.dk>
2026-02-16 15:52:34 +0100

    aws

--- migration_nextjs/minitwit-frontend/next.config.ts
+++ migration_nextjs/minitwit-frontend/next.config.ts
@@ -4,7 +4,7 @@ const os = require('os');
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   env: {
-    host: "http://localhost", //+ os.hostname(),
+    host: "http://172.31.41.225", //+ os.hostname(),
     port : "5001",
     portClient : "3000"
   }
