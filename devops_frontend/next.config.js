28230141e79b6eec8b6b42eeb34362b776417bd0
alqeren <alqeren1@gmail.com>
2026-02-24 14:39:06 +0100

    changing frontend code to .js and building a UI

---
+++ devops_frontend/next.config.js
@@ -0,0 +1,13 @@
+const os = require("os");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  env: {
+    host: "http://ec2-13-51-198-31.eu-north-1.compute.amazonaws.com",
+    port: "5001",
+    portClient: "3000",
+  },
+};
+
+module.exports = nextConfig;
