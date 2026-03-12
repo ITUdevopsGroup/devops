d4adc48d9dd0de23d67834fa0fa022a7f539ffa6
alqeren <alqeren1@gmail.com>
2026-03-12 17:29:41 +0100

    postgresql version api fixed

--- devops_backend/src/main/java/com/devops/itu_minitwit/repository/MessageRepository.java
+++ devops_backend/src/main/java/com/devops/itu_minitwit/repository/MessageRepository.java
@@ -3,6 +3,7 @@ package com.devops.itu_minitwit.repository;
 import java.util.Collection;
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devops.itu_minitwit.domain.Message;
@@ -12,11 +13,15 @@ public interface MessageRepository extends JpaRepository<Message, Integer> {
 
   long countByFlagged(Integer flagged);
 
-  List<Message> findByFlaggedOrderByPubDateDesc(Integer flagged);
+  List<Message> findByFlaggedOrderByPubDateDesc(Integer flagged, Pageable pageable);
 
-  List<Message> findByFlaggedAndAuthorInOrderByPubDateDesc(Integer flagged, Collection<User> authors);
+  List<Message> findByFlaggedAndAuthorInOrderByPubDateDesc(
+      Integer flagged,
+      Collection<User> authors,
+      Pageable pageable);
 
-  List<Message> findByFlaggedAndAuthorUsernameOrderByPubDateDesc(Integer flagged, String username);
+  List<Message> findByFlaggedAndAuthorUsernameOrderByPubDateDesc(
+      Integer flagged,
+      String username,
+      Pageable pageable);
 }
-
-
