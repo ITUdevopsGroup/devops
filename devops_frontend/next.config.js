11b71349d8c56f11f064bf3fb9c2efd4ba24b035
Maria <mariamoeller@live.dk>
2026-03-28 11:43:52 +0100

    fix frontend config

--- devops_frontend/next.config.js
+++ devops_frontend/next.config.js
@@ -4,7 +4,7 @@ const os = require("os");
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    host: "http://167.172.111.97",
+    host: process.env.NEXT_PUBLIC_API_HOST || "http://167.172.111.97",
     port: "5001",
     portClient: "3000",
   },
