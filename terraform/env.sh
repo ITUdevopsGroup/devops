efe344edbae9dd8c6656f068380f53bcd4ddd8ca
Anders Nielsen <andersfrimann@hotmail.dk>
2026-04-10 16:11:42 +0200

    Terraform init

--- terraform/env.sh
+++ terraform/env.sh
@@ -7,3 +7,8 @@ export PUB_KEY="$HOME/.ssh/xxx.pub"
 export DIGITALOCEAN_SSH_KEY="terraform"
 export DO_PAT="xxx"
 export TF_LOG="INFO"
+
+# Run following commands to install infra
+# terraform init
+# terraform plan   -var "do_token=${DO_PAT}"   -var "pvt_key=${PVT_KEY}"   -var "pub_key=${PUB_KEY}"   -var "do_ssh_key_name=${DIGITALOCEAN_SSH_KEY}"   -var "droplet_size=${DROPLET_SIZE}"
+# terraform apply   -var "do_token=${DO_PAT}"   -var "pvt_key=${PVT_KEY}"   -var "pub_key=${PUB_KEY}"   -var "do_ssh_key_name=${DIGITALOCEAN_SSH_KEY}"   -var "droplet_size=${DROPLET_SIZE}"
