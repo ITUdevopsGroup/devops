7e61846174db0402567750e82f11d6b3fbb719d9
Anders Nielsen <andersfrimann@hotmail.dk>
2026-04-10 15:03:32 +0200

    Terraform initial draft

---
+++ terraform/env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x
+echo "Initializing environment for Terraform"
+export DROPLET_SIZE="s-1vcpu-512mb-50gb" # You can select a droplet size from https://slugs.do-api.dev/
+export PVT_KEY="$HOME/.ssh/xxx"
+export PUB_KEY="$HOME/.ssh/xxx.pub"
+export DIGITALOCEAN_SSH_KEY="terraform"
+export DO_PAT="xxx"
+export TF_LOG="INFO"
