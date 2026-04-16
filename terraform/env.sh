4eb556060c2d5a6904108d789c9658066c4ed2af
Anders Nielsen <andersfrimann@hotmail.dk>
2026-04-16 14:25:55 +0200

    Minor

--- terraform/env.sh
+++ terraform/env.sh
@@ -9,7 +9,7 @@ export DO_PAT="<Create access token and paste value into here>"
 export TF_LOG="INFO"
 
 # Install Terraform from https://developer.hashicorp.com/terraform/install
-# Run following commands to install infra
+# Run following commands to install infra from within this folder
 # terraform init
 # terraform plan   -var "do_token=${DO_PAT}"   -var "pvt_key=${PVT_KEY}"   -var "pub_key=${PUB_KEY}"   -var "do_ssh_key_name=${DIGITALOCEAN_SSH_KEY}"   -var "droplet_size=${DROPLET_SIZE}"
 # terraform apply   -var "do_token=${DO_PAT}"   -var "pvt_key=${PVT_KEY}"   -var "pub_key=${PUB_KEY}"   -var "do_ssh_key_name=${DIGITALOCEAN_SSH_KEY}"   -var "droplet_size=${DROPLET_SIZE}"
